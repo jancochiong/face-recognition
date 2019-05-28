60d81f75fb9ca98aba06bf6d9d86032360a19cdc
Jan Co Chiong <jan.cochiong@gmail.com>
2019-05-28 02:27:03 -0400

    use clarify for face detection

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Particles from 'react-particles-js'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Rank from './components/Rank/Rank'
+import Clarifai from 'clarifai'
 import './App.css'
 import 'tachyons'
+
+const app = new Clarifai.App({
+	apiKey: '2397e3acfa124370b21c4e4170540449'
+});
 
 const particlesOptions = {
 	particles: {
@@ -20,6 +26,28 @@ const particlesOptions = {
 }
 
 class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			input: '',
+			imageUrl: ''
+		}
+	}
+	onInputChange = (event) => {
+		this.setState({input: event.target.value});
+	}
+
+	onButtonSubmit = () =>{
+		this.setState({imageUrl: this.state.input});
+		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+		function(response) {
+			console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+		},
+		function(err) {
+			
+		}
+		);
+	}
 	render(){
 		return (
 			<div className="App">
@@ -28,9 +56,12 @@ class App extends Component {
 				/>
 				<Navigation/>
 				<Logo/>
-				<Rank />
-				<ImageLinkForm/>
-				{/* <FaceRecognition/> */}
+				<Rank/>
+				<ImageLinkForm 
+					onInputChange={this.onInputChange} 
+					onButtonSubmit={this.onButtonSubmit}
+				/>
+				<FaceRecognition imageUrl={this.state.imageUrl}/>
 			</div>
 		)
 	};
