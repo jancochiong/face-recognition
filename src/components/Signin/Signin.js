2e7504709ba6df0eb78592e7be2cae3edc39134e
Jan Co Chiong <jan.cochiong@gmail.com>
2019-06-01 22:42:41 -0400

    test

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -21,7 +21,7 @@ const Signin = () => {
                         <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
                     </div>
                     <div className="lh-copy mt3">
-                        <a href="#0" className="f6 link dim black db">Register</a>
+                        <a href="#0" className="f6 link dim black db">Register123</a>
                     </div>
                 </form>
             </main>
